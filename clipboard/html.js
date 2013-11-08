353aada9774d3d7c3a1a51b3153d39617df2f429
Carabiner Systems, Inc <carabiner0621@gmail.com>
2013-11-08 12:02:29 +0900

    Add an example to save to clipboard as HTML

---
+++ clipboard/html.js
@@ -0,0 +1,157 @@
+////////////////////////////////////////////////////////////////////
+// HTML+Clipboardスクリプト
+////////////////////////////////////////////////////////////////////
+importPackage(java.lang);
+importPackage(java.util);
+importPackage(java.io);
+importPackage(java.awt);
+importPackage(java.awt.datatransfer);
+
+// HTML化します。
+var htmlContent = htmlOf(window.selectedNodes[0], 0, 0, true);
+var html = "<html><body>" + htmlContent + "</body></html>";
+
+System.out.println(html);
+
+var clipboard = new Clipboard("text/html");
+clipboard.copy(html);
+
+
+// HTML化します。
+function htmlOf(node, headerLevel, currentIndex) {
+	var r = "";
+	var childHeaderLevel = headerLevel;
+	var tagset = null;
+
+	// 見出し
+	if(isHeader(node)) {
+		childHeaderLevel = headerLevel + 1;
+		var tagIndex = headerLevel + 1;
+		var tag = "h" + tagIndex;
+		tagset = {startTag: "<" + tag + ">", endTag: "</" + tag + ">",
+				startChildTag: "", endChildTag: "", startChildListTag: "", endChildListTag: "", keepContentPTag: false}
+	}else{
+		childHeaderLevel = 0;
+	}
+
+	// (1), 1.など
+	itemizeTagset = getItemizeTagset(node);
+	if(itemizeTagset != null) {
+		tagset = itemizeTagset;
+	}
+
+	// 本体の出力
+	var keepContentPTag = true;
+	if(tagset != null) {
+		r += tagset.startTag;
+		keepContentPTag = tagset.keepContentPTag;
+	}
+	r += htmlContentOf(node, keepContentPTag);
+	if(tagset != null) {
+		r += tagset.endTag;
+		r += tagset.startChildListTag;
+	}
+	var childNodes = node.childNodes;
+	var index = 0;
+	for(var i = 0; i < childNodes.length; i ++) {
+		var childNode = childNodes[i];
+		if(tagset != null) {
+			r += tagset.startChildTag;
+		}
+		r += htmlOf(childNode, childHeaderLevel, index);
+		if(tagset != null) {
+			r += tagset.endChildTag;
+		}
+		if(childNode.nodeType == "topic") {
+			index ++;			
+		}
+	}
+	if(tagset != null) {
+		r += tagset.endChildListTag;
+	}
+	return r;
+}
+
+// 見出しかどうかを判別(n.n.n.の形式のみ見出しと判別)
+function isHeader(node) {
+	// topicTypeの定数
+	// http://beitel.carabiner.jp/javadoc/jp/carabiner/treeeditor/TreeEditorConst.html#NUMBER_SECTION
+	if(node.topicType == 1 && node.nodeType == "topic") {
+		return true;
+	}
+	return false;
+}
+
+// 箇条書きのタグセットの取得
+function getItemizeTagset(node) {
+	var childTopicType = null;
+	var childNodes = node.childNodes;
+	for(var i = 0; i < childNodes.length; i ++) {
+		var childNode = childNodes[i];
+		if(childNode.nodeType == "topic") {
+			childTopicType = childNode.topicType;
+		}
+	}
+
+	if(childTopicType == null) {
+		return null;
+	}else if(childTopicType == 11) {
+		// (n)
+		return {startTag: "", endTag: "", startChildTag: "<li>", endChildTag: "</li>", startChildListTag: "<ol>", endChildListTag: "</ol>", keepContentPTag: true}
+	}
+	return null;
+}
+
+// ノード自身のHTMLコンテンツの取得
+function htmlContentOf(node, keepContentPTag) {
+	if(keepContentPTag) {
+		return convertMacro(node.html);
+	}
+	var html = new String(node.html);
+	var startTagPattern = /<p[^>]*>/g;
+	html = html.replace(startTagPattern, "");
+	
+	var endTagPattern = /<\/p>/g;
+	html = html.replace(endTagPattern, "");
+	
+	return convertMacro(html);
+}
+
+function convertMacro(html) {
+	var specialChPattern = /\&[^;]+;/g;
+	html = html.replace(specialChPattern, "");
+	return html;
+}
+
+// Clipboardの処理を実装します。
+function Clipboard(mimeType) {
+
+	this.copy = function(value) {
+			var flavors = new ArrayList();
+			flavors.add(new DataFlavor(mimeType + ";class=java.lang.String"));
+			flavors.add(new DataFlavor(mimeType + ";class=java.io.Reader"));
+			flavors.add(new DataFlavor(mimeType + ";charset=unicode;class=java.io.InputStream"));
+			
+			var trans = new Transferable({
+				getTransferDataFlavors: function() {
+					return flavors.toArray(java.lang.reflect.Array.newInstance(java.awt.datatransfer.DataFlavor, 0));
+				},
+				isDataFlavorSupported: function(flavor) {
+					return flavors.contains(flavor);
+				},
+				getTransferData: function(flavor) {
+					var className = flavor.getRepresentationClass().name;
+					if (className == "java.lang.String") {
+						return value;
+					} else if (className == "java.io.StringReader") {
+						return new StringReader(value);
+					} else if (className == "java.io.InputStream") {
+						return new StringBufferInputStream(value);
+					}
+					throw new UnsupportedFlavorException(flavor);
+				}
+			});
+			Toolkit.getDefaultToolkit().getSystemClipboard().setContents(trans, null);
+		};
+	
+}
